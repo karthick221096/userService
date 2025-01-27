bcaa00e6aeb43543ff94305a700f267dabe2e746
karthick221096 <49914819+karthick221096@users.noreply.github.com>
2025-01-27 07:50:39 +0530

    Override default securityConfiguration

---
+++ src/main/java/com/karthick/userservice/configruations/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.karthick.userservice.configruations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((request) -> {
+            try {
+                request
+                        .anyRequest()
+                        .permitAll()
+                        .and()
+                        .cors().disable()
+                        .csrf().disable();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return http.build();
+    }
+}
